58e4de629dbfa8cd68e79aa9500dfe30c1f84c93
Lucas <lucasdeters@gmail.com>
2023-06-14 16:32:47 -0300

    changed discovery

--- src/utils/discovery.ts
+++ src/utils/discovery.ts
@@ -1,40 +1,40 @@
-import { AxiosInstance } from 'axios';
-
 interface IUrlList {
-  [key: string]: string;
+  [key: string]: string
 }
 
 export default class Discovery {
-  client: AxiosInstance;
-  proxyListUrl: IUrlList;
-  proxyListAppUrl: IUrlList;
+  client: typeof fetch
+  proxyListUrl: IUrlList
+  proxyListAppUrl: IUrlList
 
-  DISCOVERY_URL = 'https://prod-s0-webapp-proxy.nubank.com.br/api/discovery';
-  DISCOVERY_APP_URL = 'https://prod-s0-webapp-proxy.nubank.com.br/api/app/discovery';
+  DISCOVERY_URL = "https://prod-s0-webapp-proxy.nubank.com.br/api/discovery"
+  DISCOVERY_APP_URL =
+    "https://prod-s0-webapp-proxy.nubank.com.br/api/app/discovery"
 
-  constructor(client: AxiosInstance) {
-    this.client = client;
-    this.updateProxyUrls();
+  constructor(client: typeof fetch) {
+    this.client = client
+    this.updateProxyUrls()
   }
 
   getUrl(name: string, target?: IUrlList): string {
-    const urlList = (target || this.proxyListUrl);
-    const url: string = urlList[name];
+    const urlList = target || this.proxyListUrl
+    const url: string = urlList[name]
 
     if (!url) {
-      throw new Error(`There is no URL discovered for ${name}`);
+      throw new Error(`There is no URL discovered for ${name}`)
     }
 
-    return url;
+    return url
   }
 
   getAppUrl(name: string): string {
-    return this.getUrl(name, this.proxyListAppUrl);
+    return this.getUrl(name, this.proxyListAppUrl)
   }
 
   async updateProxyUrls() {
-    this.proxyListUrl = (await this.client.get(this.DISCOVERY_URL)).data;
-    this.proxyListAppUrl = (await this.client.get(this.DISCOVERY_APP_URL)).data;
+    this.proxyListUrl = await (await this.client(this.DISCOVERY_URL)).json()
+    this.proxyListAppUrl = await (
+      await this.client(this.DISCOVERY_APP_URL)
+    ).json()
   }
-
 }
