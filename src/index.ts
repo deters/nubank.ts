e83f178a42fc41040618e42026554d2914308efb
Lucas <lucasdeters@gmail.com>
2023-06-14 17:10:48 -0300

    fixed buffer

--- src/index.ts
+++ src/index.ts
@@ -78,7 +78,7 @@ export default class NubankTS implements INubankTS {
     this.user = user
     this.password = password
 
-    this.certificate = Buffer(Deno.readFile(certificatePath))
+    this.certificate = new Buffer(Deno.readFile(certificatePath))
     this.discovery = new Discovery()
     this.loadMe()
   }
